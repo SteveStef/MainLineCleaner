dd9220a12d28e0f1bceba0cab3f8fe1b60718b5e
stevestef <stephenstef456@gmail.com>
2025-05-07 20:17:40 -0400

    Getting ready for cloudflare2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 // next.config.js
+
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',         // static HTML export
@@ -12,5 +14,9 @@ const nextConfig = {
     unoptimized: true,      // you can even remove this if you’re not using next/image
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
