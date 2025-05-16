fd281b45ea73b92e786c94182a5ee8ad0365f2b5
Stephen Stefanatos <stephenstef456@gmail.com>
2025-05-16 18:29:13 -0400

    Fixing middleware errors

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,25 @@ export async function middleware(req: NextRequest) {
     if (req.cookies.has('tempauthtoken')) {
         return NextResponse.next();
     }
-
-    const tokenRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/token`);
-    if (!tokenRes.ok) {
-        return NextResponse.next();
-    }
-    const token = await tokenRes.text();
+    let token = null;
+    try {
+        const tokenRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/token`);
+        if (!tokenRes.ok) {
+            return NextResponse.next();
+        }
+        token = await tokenRes.text();
+    } catch(err) {}
     const res = NextResponse.next();
-    res.cookies.set({
-        name:  'tempauthtoken',
-        value: token,
-        httpOnly: false, // makes it so that my
-        sameSite: 'none',
-        secure: true,
-        //maxAge: 60
-    });
+    if(token){
+        res.cookies.set({
+            name: 'tempauthtoken',
+            value: token,
+            httpOnly: false, // makes it so that my
+            sameSite: 'none',
+            secure: true,
+            //maxAge: 60
+        });
+    }
     return res;
 }
 
